f08831ed126be02cca209c04489b41b83ef380f8
Natalia Kepa <kepanatalialu@gmail.com>
2020-03-07 10:27:26 +0100

    Added module 3.3

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -1,53 +1,67 @@
 // Importowanie biblioteki potrzebnej do losowania liczb
 import java.util.Random;
-import java.util.Arrays;
 
 public class RandomNumbers {
+    int maxNum = 0;
+    int minNum = 0;
+    int currentNum = 0;
 
-    public static void main(String[] args) {
-
-        int[] results = getCountOfRandomNumber();
-        int biggestNumber = results[0];
-        int smallestNumber = results[1];
-
-        System.out.println("Największa wylosowana liczba to: " + biggestNumber);
-        System.out.println("Największa wylosowana liczba to: " + smallestNumber);
-
-//      wydrukuj tablicę z najmniejszą i największą wylosowaną liczbą
-        System.out.println(Arrays.toString(results));
+    // metoda do wylosowania jednej liczby
+    public int diceOneNumber() {
+        Random random = new Random();
+        int currentNum = random.nextInt(30) + 1;
+        this.currentNum = currentNum;
+        return currentNum;
     }
 
-    //    metoda do losowania liczb
-    public static int[] getCountOfRandomNumber() {
-        Random random = new Random();
+
+    //    metoda do sprawdzenia czy wylosowana liczba jest mniejsza od poprzedniej
+    public int ifMinNum() {
+        int temporaryNum = currentNum;  // przypisanie aktualnie wylosowanej liczby do zmiennej
+        if (minNum > temporaryNum) {  // porównanie aktualnie wylosowanej liczy z poprzednią
+            minNum = temporaryNum;
+        }
+        return minNum;
+    }
+
+    //    metoda do sprawdzenia czy wylosowana liczba jest większa od poprzedniej
+    public int ifMaxNum() {
+        int temporaryNum = currentNum;  // przypisanie aktualnie wylosowanej liczby do zmiennej
+        if (maxNum < temporaryNum) {    // porównanie aktualnie wylosowanej liczy z poprzednią
+            maxNum = temporaryNum;
+        }
+        return maxNum;
+    }
+
+    //  metoda do petli wykonującej się, aż suma dodanych liczb przekroczy 5000
+    public int diceWhile() {
         int sum = 0;
-        int biggestNumber = 0;
-        int smallestNumber = 0;
-
-//      pierwsze losowanie
-        int temporaryNum = random.nextInt(30)+1;
-        smallestNumber = temporaryNum;
-        biggestNumber = temporaryNum;
-
-        sum = sum + temporaryNum;
-
-//      pozostałe losowania
         while (sum <= 5000) {
-            temporaryNum = random.nextInt(30)+1;
-
-//          zapisz największą wylosowaną wartość
-            if (biggestNumber < temporaryNum) {
-                biggestNumber = temporaryNum;
-            }
-
-//          zapisz najmniejszą wylosowaną wartość
-            if (smallestNumber > temporaryNum) {
-                smallestNumber = temporaryNum;
-            }
+            int temporaryNum = diceOneNumber();  //uruchomienie metody do losowania pojedynczej losowej liczby
+            ifMaxNum(); // uruchomienie metody do sprawdzenia czy wylosowana liczba jest większa od poprzedniej
+            ifMinNum(); // uruchomienie metody do sprawdzenia czy wylosowana liczba jest mniejsza od poprzedniej
             sum = sum + temporaryNum;
         }
-//      zwróć tablicę z najmniejszą i największą wylosowaną liczbą
-        return new int[] {smallestNumber, biggestNumber};
+        return sum;
     }
+
+    //    uruchomienie metod
+    public static void main(String[] args) {
+
+        RandomNumbers number = new RandomNumbers();
+        int total = number.diceWhile();
+
+        int maxNum = number.ifMaxNum();
+        System.out.println("Ostateczna maxNum: " + maxNum);
+
+        int minNum = number.ifMinNum();
+        System.out.println("Ostateczna minNum: " + minNum);
+
+        System.out.println("Ostateczna suma z losowań: " + total);
+
+    }
+
+
+
 
 }
